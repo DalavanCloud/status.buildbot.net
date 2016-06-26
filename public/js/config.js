ada182874d99211e45d84a4b9b1bf440d8208530
Dustin J. Mitchell <dustin@buildbot.net>
2016-06-26 20:26:40 +0000

    add API keys for buildbot

---
+++ public/js/config.js
@@ -0,0 +1,18 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm777782197-9595e20f929efbe647abf793',  // trac
+	'm777782205-679c0fc63ab55935b6070887',  // service3
+	'm777782204-486e4e4a3b99d3d2f48603e7',  // service2
+	'm777782202-db856fd8752df94e9bf1272c',  // service1
+	'm777782199-e5d0cdfce33a4e36489f055c',  // metabuildbot
+	'm777782200-6722ea9dcd903144dc718665',  // lists
+	'm777782201-dd6266a1e0bff2bcd7e82008',  // docs
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
